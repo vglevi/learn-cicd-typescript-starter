af7d5be893430f0d9fb297248a729f8825cb5f6c
vglevi <zsovalevente@gmail.com>
2026-03-29 15:35:05 +0200

    fixing getAPIKey

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return "null";
+    return null;
   }
 
   const splitAuth = authHeader.split(" ");
